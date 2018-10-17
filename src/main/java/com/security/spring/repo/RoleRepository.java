13b48ef506fac3f2b09b374740b12a1c9a389c49
George Brande <george.brande@gmail.com>
2018-10-17 13:28:11 +0300

    final OK

---
+++ src/main/java/com/security/spring/repo/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.security.spring.repo;
+
+import com.security.spring.models.Role;
+import com.security.spring.models.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
---
+++ src/main/java/com/security/spring/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.security.spring.security.services;
+
+import com.security.spring.models.User;
+import com.security.spring.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("user not found with username: " + username));
+        return UserPrinciple.build(user);
+    }
+}
